58f8075277e3d3c31dd9325b3c370bfeda87635f
Ricardo <ricardinhogiasson16@gmail.com>
2021-02-02 02:06:14 -0300

    Add ulr class

--- ProjetoByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ProjetoByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -12,12 +12,37 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            DateTime dataLimite = new DateTime(2021, 5, 23);
-            DateTime dataAtual = DateTime.Now;//CAPTAMOS A DATA ATUAL E O HORÁRIO
-            TimeSpan diferenca =(dataLimite - dataAtual); //dataFimPagamento - dataCorrente;
-            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
-            Console.WriteLine(mensagem);
+            string url = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDEstino=dolar";
+            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL(url);
+            string valor = (extrator.GetValor("moeda"));
+            Console.WriteLine("Valor da moeda Destino: " + valor);
+
+            valor = (extrator.GetValor("moedaOrigem"));
+            Console.WriteLine("Valor da moeda Origem : " + valor);
+            
             Console.ReadLine();
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*          DateTime dataLimite = new DateTime(2021, 5, 23);
+            DateTime dataAtual = DateTime.Now;//CAPTAMOS A DATA ATUAL E O HORÁRIO
+            TimeSpan diferenca =(dataLimite - dataAtual); //dataFimPagamento - dataCorrente;
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            Console.WriteLine(mensagem);*/
